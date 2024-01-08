48ccb6cd55d2f1083f3bc3df16f2e34b7f7a2a78
Rupali <rupalichauhan022@gmail.com>
2024-01-08 23:18:23 +0530

    Adding javascript file

--- first.js
+++ first.js
@@ -1,9 +1,6 @@
-const student={
-    name:"Rupali",
-    age:25,
-    cgpa:8.45
-};
-student["age"]=student["age"]+1;
-student["name"]="Rupali Chauhan";
-console.log(student.name);
-console.log(student["age"]);
+let num="5";
+let sum=5;
+
+let thum=(num==sum)?"true":"false";
+
+console.log(thum);
