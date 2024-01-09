abaf7024fb2334b1e99dc625d66c51bd9cf35026
Rupali <rupalichauhan022@gmail.com>
2024-01-09 23:07:50 +0530

    javascript file updating 09-01-2024

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
-let num="5";
-let sum=5;
-
-let thum=(num==sum)?"true":"false";
-
-console.log(thum)
+let num=prompt("Enter a number = ");
+if (num%5==0){
+    console.log("Number is divisible by 5");
+}else{
+    console.log("Number is not divisible by 5");
+}
