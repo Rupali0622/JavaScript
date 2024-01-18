a89dd53337eb9bce214828eb84677180b2aabe75
Rupali <rupalichauhan022@gmail.com>
2024-01-18 20:30:29 +0530

    Map,Filter

--- CheckVowelsInString.js
+++ CheckVowelsInString.js
@@ -1,11 +1,21 @@
-function checkVowels(str){
-    let count=0;
-    for(let char of str){
-        if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u" || char==="A" || char==="E" || char==="I" || char==="O" || char==="U"){
+function checkVowels(str) {
+    let count = 0;
+    for (let char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u" || char === "A" || char === "E" || char === "I" || char === "O" || char === "U") {
             count++;
         }
     }
     console.log(count);
 }
-
 checkVowels("Jai Shree Ram");
+
+let arrowfunc = (str) => {
+    let count = 0;
+    for (let char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u" || char === "A" || char === "E" || char === "I" || char === "O" || char === "U") {
+            count++;
+        }
+    }
+    console.log(count);
+}
+arrowfunc("Jai Shree Ram");
